3f8af7f9de7c93973dc0c3d69914ab7d07bdac92
PBara7a <pbara7a@gmail.com>
2023-11-03 22:11:47 +0000

    It can process text from a file or from stdin

--- ccwc.py
+++ ccwc.py
@@ -1,4 +1,6 @@
+#!/usr/bin/env python3
 import os
+import sys
 import argparse
 import locale
 
@@ -7,79 +9,66 @@ def is_multibyte_encoding_supported() -> bool:
     multibyte_encodings = ['utf-8', 'utf-16', 'utf-32']
     return any(encoding in locale_encoding.lower() for encoding in multibyte_encodings)
 
-def count_bytes(filename: str) -> int:
-    """Function to count the bytes in a file."""
-    try:
-        return os.path.getsize(filename)
-    except FileNotFoundError:
-        print(f"The file {filename} was not found.")
-        return -1
-    except OSError as e:
-        print(f"An error occurred: {e}")
-        return -1
+def count_bytes(data: str) -> int:
+    """Function to count the bytes in a string."""
+    return len(data.encode('utf-8'))
     
-def count_lines(filename: str) -> int:
-    """Function to count the number of lines in a file."""
-    try:
-        with open(filename, 'r') as file:
-            return sum(1 for line in file)
-    except FileNotFoundError:
-        print(f"The file {filename} was not found.")
-        return -1
-    except OSError as e:
-        print(f"An error occurred: {e}")
-        return -1
+def count_lines(data: str) -> int:
+    """Function to count the number of lines in a string."""
+    return data.count('\n')
     
-def count_words(filename: str) -> int:
-    """Function to count the number of words in a file."""
-    try:
-        with open(filename, 'r') as file:
-            return sum(len(line.split()) for line in file)
-    except FileNotFoundError:
-        print(f"The file {filename} was not found.")
-        return -1
-    except OSError as e:
-        print(f"An error occurred: {e}")
-        return -1
+def count_words(data: str) -> int:
+    """Function to count the number of words in a string."""
+    return len(data.split())
     
-def count_characters(filename: str) -> int:
+def count_characters(data: str) -> int:
     """Function to count the number of characters in a file.
        If the current locale does not support multibyte characters,
        it returns a count of bytes instead."""
     if not is_multibyte_encoding_supported():
-        return count_bytes(filename)
-    try:
-        with open(filename, 'r') as file:
-            return sum(len(line) for line in file)
-    except FileNotFoundError:
-        print(f"The file {filename} was not found.")
-        return -1
-    except OSError as e:
-        print(f"An error occurred: {e}")
-        return -1
+        return count_bytes(data)
+    return len(data)
+
+def read_from_stdin() -> str:
+    return sys.stdin.read()
+
+def read_from_file(filename: str) -> str:
+    return open(filename, 'r').read()
 
 def main():
     parser = argparse.ArgumentParser(prog='wc', description='A clone of the Linux tool wc')
-    parser.add_argument('filename', type=str, help='File to process')
+    parser.add_argument('filename', nargs='?', type=str, help='File to process')
     parser.add_argument('-c', '--count', action='store_true', help='Count the bytes in the file')
     parser.add_argument('-l', '--lines', action='store_true', help='Count the lines in the file')
     parser.add_argument('-w', '--words', action='store_true', help='Count the words in the file')
     parser.add_argument('-m', '--multibyte', action='store_true', help='Count the multibyte characters in the file')
-
     args = parser.parse_args()
 
-    byte_count, line_count, word_count, multibyte_count = '', '', '', ''
+    filename, byte_count, line_count, word_count = '', '', '', ''
+    flags_passed = any([args.count, args.lines, args.words, args.multibyte])
 
-    if args.count and byte_count != -1:
-        byte_count = count_bytes(args.filename)
-    if args.lines and line_count != -1:
-        line_count = count_lines(args.filename)
-    if args.words and word_count != -1:
-        word_count = count_words(args.filename)
-    if args.multibyte and multibyte_count != -1:
-        byte_count = count_characters(args.filename) # The -m option cancels a prior usage of -c
+    data = None
+    if args.filename:
+        filename = args.filename
+        data = read_from_file(args.filename)
+    else:
+        data = read_from_stdin()
 
-    print(f"{line_count} {word_count} {byte_count} {args.filename}")
+    if not flags_passed:
+        byte_count = count_bytes(data)
+        line_count = count_lines(data)
+        word_count = count_words(data)
+    else:
+        if args.count:
+            byte_count = count_bytes(data) # The -c option cancels a prior usage of -m (not done yet)
+        if args.lines:
+            line_count = count_lines(data)
+        if args.words:
+            word_count = count_words(data)
+        if args.multibyte:
+            byte_count = count_characters(data)  # The -m option cancels a prior usage of -c
+
+    print(f"{line_count} {word_count} {byte_count} {filename}")
 
 if __name__ == '__main__':
     main()
--- test_ccwc.py
+++ test_ccwc.py
@@ -3,69 +3,68 @@ import subprocess
 from unittest.mock import patch
 from ccwc import count_bytes, count_lines, count_words, count_characters
 
+filename = 'test.txt'
+data = open(filename, 'r').read()
+data_from_stdin = subprocess.check_output(['cat', filename]).decode('utf-8')
+
 class TestSPC(unittest.TestCase):
     """Tests will be done against wc, the Linux tool"""
 
     def test_byte_count(self):
-        filename = 'test.txt'
-        
         # Get the byte count from my application
-        ccwc_byte_count = count_bytes(filename)
+        ccwc_byte_count = count_bytes(data)
+        ccwc_stdin_byte_count = count_bytes(data_from_stdin)
         
         # Get the byte count from wc
         wc_output = subprocess.check_output(['wc', '-c', filename]).decode('utf-8')
         wc_byte_count = int(wc_output.strip().split()[0])
         
-        self.assertEqual(ccwc_byte_count, wc_byte_count, "Byte count does not match wc's output")
-
-    def test_byte_count_missing_file(self):
-        filename = 'missing.txt'
-        ccwc_byte_count = count_bytes(filename)
-        expected_count = -1
-        self.assertEqual(ccwc_byte_count, expected_count, "Byte count does not match expected")
+        self.assertEqual(ccwc_byte_count, wc_byte_count, "(File) Byte count does not match wc's output")
+        self.assertEqual(ccwc_stdin_byte_count, wc_byte_count, "(stdin) Byte count does not match wc's output")
 
     def test_line_count(self):
-        filename = 'test.txt'
-        
-        ccwc_line_count = count_lines(filename)
+        ccwc_line_count = count_lines(data)
+        ccwc_stdin_line_count = count_lines(data_from_stdin)
         
         wc_output = subprocess.check_output(['wc', '-l', filename]).decode('utf-8')
         wc_byte_count = int(wc_output.strip().split()[0])
         
-        self.assertEqual(ccwc_line_count, wc_byte_count, "Line count does not match wc's output")
+        self.assertEqual(ccwc_line_count, wc_byte_count, "(File) Line count does not match wc's output")
+        self.assertEqual(ccwc_stdin_line_count, wc_byte_count, "(stdin) Line count does not match wc's output")
 
     def test_word_count(self):
-        filename = 'test.txt'
-        
-        ccwc_word_count = count_words(filename)
+        ccwc_word_count = count_words(data)
+        ccwc_stdin_word_count = count_words(data_from_stdin)
         
         wc_output = subprocess.check_output(['wc', '-w', filename]).decode('utf-8')
         wc_byte_count = int(wc_output.strip().split()[0])
         
-        self.assertEqual(ccwc_word_count, wc_byte_count, "Word count does not match wc's output")
+        self.assertEqual(ccwc_word_count, wc_byte_count, "(File) Word count does not match wc's output")
+        self.assertEqual(ccwc_stdin_word_count, wc_byte_count, "(stdin) Word count does not match wc's output")
 
     def test_character_count(self):
-        filename = 'test.txt'
-        
-        ccwc_character_count = count_characters(filename)
+        ccwc_character_count = count_characters(data)
+        ccwc_stdin_character_count = count_characters(data_from_stdin)
         
         wc_output = subprocess.check_output(['wc', '-m', filename]).decode('utf-8')
         wc_byte_count = int(wc_output.strip().split()[0])
         
-        self.assertEqual(ccwc_character_count, wc_byte_count, "Character count does not match wc's output")
+        self.assertEqual(ccwc_character_count, wc_byte_count, "(File) Character count does not match wc's output")
+        self.assertEqual(ccwc_stdin_character_count, wc_byte_count, "(stdin) Character count does not match wc's output")
 
     @patch('locale.getpreferredencoding')
     def test_character_count_multibyte_not_supported(self, mock_getpreferredencoding):
         mock_getpreferredencoding.return_value = 'nope'
-        filename = 'test.txt'
-        
-        ccwc_character_count = count_characters(filename)
+
+        ccwc_character_count = count_characters(data)
+        ccwc_stdin_character_count = count_characters(data_from_stdin)
         
         # It should match the byte count instead
         wc_output = subprocess.check_output(['wc', '-c', filename]).decode('utf-8')
         wc_byte_count = int(wc_output.strip().split()[0])
         
-        self.assertEqual(ccwc_character_count, wc_byte_count, "Character count does not match wc's output")
+        self.assertEqual(ccwc_character_count, wc_byte_count, "(File) Character count does not match wc's output")
+        self.assertEqual(ccwc_stdin_character_count, wc_byte_count, "(stdin) Character count does not match wc's output")
 
 if __name__ == '__main__':
     unittest.main()
