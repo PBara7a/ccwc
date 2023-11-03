0a7d64af118588d24697ed2e54466c36fd8713e0
PBara7a <pbara7a@gmail.com>
2023-11-03 19:59:31 +0000

    Counts words in a file

--- ccwc.py
+++ ccwc.py
@@ -23,23 +23,38 @@ def count_lines(filename: str) -> int:
     except OSError as e:
         print(f"An error occurred: {e}")
         return -1
+    
+def count_words(filename: str) -> int:
+    """Function to count the number of words in a file."""
+    try:
+        with open(filename, 'r') as file:
+            return sum(len(line.split()) for line in file)
+    except FileNotFoundError:
+        print(f"The file {filename} was not found.")
+        return -1
+    except OSError as e:
+        print(f"An error occurred: {e}")
+        return -1
 
 def main():
     parser = argparse.ArgumentParser(prog='wc', description='A clone of the Linux tool wc')
     parser.add_argument('filename', type=str, help='File to process')
     parser.add_argument('-c', '--count', action='store_true', help='Count the bytes in the file')
     parser.add_argument('-l', '--lines', action='store_true', help='Count the lines in the file')
+    parser.add_argument('-w', '--words', action='store_true', help='Count the words in the file')
 
     args = parser.parse_args()
 
-    byte_count, line_count = "", ""
+    byte_count, line_count, word_count = '', '', ''
 
     if args.count and byte_count != -1:
         byte_count = count_bytes(args.filename)
     if args.lines and line_count != -1:
         line_count = count_lines(args.filename)
+    if args.words and word_count != -1:
+        word_count = count_words(args.filename)
 
-    print(f"{line_count} {byte_count} {args.filename}")
+    print(f"{line_count} {word_count} {byte_count} {args.filename}")
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
--- test_ccwc.py
+++ test_ccwc.py
@@ -1,6 +1,6 @@
 import unittest
 import subprocess
-from ccwc import count_bytes, count_lines
+from ccwc import count_bytes, count_lines, count_words
 
 class TestSPC(unittest.TestCase):
     """Tests will be done against wc, the Linux tool"""
@@ -33,11 +33,15 @@ class TestSPC(unittest.TestCase):
         
         self.assertEqual(ccwc_line_count, wc_byte_count, "Line count does not match wc's output")
 
-    def test_line_count_missing_file(self):
-        filename = 'missing.txt'
-        ccwc_line_count = count_lines(filename)
-        expected_count = -1
-        self.assertEqual(ccwc_line_count, expected_count, "Line count does not match expected")
+    def test_word_count(self):
+        filename = 'test.txt'
+        
+        ccwc_word_count = count_words(filename)
+        
+        wc_output = subprocess.check_output(['wc', '-w', filename]).decode('utf-8')
+        wc_byte_count = int(wc_output.strip().split()[0])
+        
+        self.assertEqual(ccwc_word_count, wc_byte_count, "Word count does not match wc's output")
 
 if __name__ == '__main__':
     unittest.main()
